28bafdf8bb26aff0bcb65df765eae5f3b6eb8571
gruv19 <grv1992@yandex.ru>
2022-07-16 14:30:20 +0500

    feat: Добавил шрифты google

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,11 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
